f16082ce3151488808b536477edbad685d7efe9c
osman <osman.ahmed.2022o@gmail.com>
2023-01-03 08:33:37 +0200

    os-edit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './mobile.css'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import Work from './components/Work';
+
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <Home />
-        <Work />
+        
         <Footer />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import miniPhoto from '../imgs/mini-photo.jpg'
 import myimg from '../imgs/personal-pic.jpg'
 import img1 from '../imgs/myabout.jpeg'
 import img2 from '../imgs/staff.jpg'
+import pic1 from '../imgs/my-work1.png'
+import pic2 from '../imgs/peter.png'
+import pic3 from '../imgs/dasborad.png'
+
 export default function Home(){
     return(
         <div>
@@ -16,10 +20,10 @@ export default function Home(){
                        
                        <h1 className='text-center mt-5 pt-3 job '>FRONT-END Developer <i class="bi bi-code-slash"></i></h1>
                         <div className='f-holder social-bar'>
-                            <a href='#'><i className="bi bi-instagram"></i></a>
-                            <a href='#'><i className="bi bi-twitter"></i></a>
-                            <a href='#'><i className="bi bi-linkedin"></i></a>
-                            <a href='#'><i className="bi bi-github"></i></a>
+                            <a href='#'><i className="bi bi-instagram  instagram "></i></a>
+                            <a href='#'><i className="bi bi-twitter twitter "></i></a>
+                            <a href='#'><i className="bi bi-linkedin linkedin"></i></a>
+                            <a href='#'><i className="bi bi-github github"></i></a>
                         
                        </div>
                     </div>
@@ -29,9 +33,7 @@ export default function Home(){
                     </div>
                 </div>
             </div>
-            
-            
-            <div className='container-fluid about ' id='about'>
+             <div className='container-fluid about ' id='about'>
                     <div className='row mt-5'>
                         <h1 className='heading text-center '>About Me</h1>
                     </div>
@@ -46,7 +48,40 @@ export default function Home(){
                         </div>
                     </div>
             </div>
-
+<br/>
+<br/>
+<hr/>
+            <div className = "container my-work">
+                <div className = "card-items">
+                <h1 className = "text-center title-work">MY-WORK</h1>
+            <div class="card">
+        <div class="card-content"> 
+        <h2 class="card-title">dasborad</h2>
+        <p class="card-body"> Welcome to Shirt X, the premier destination for all your clothing needs! Our store offers a wide selection of stylish and comfortable apparel for men, women, and children. Whether you’re looking for a casual t-shirt, a dress shirt for a special occasion, or something in between, we have something for everyone. We also carry a variety of accessories, including hats, scarves, and jewelry.</p>
         </div>
+        </div>
+        </div>
+
+        <div className = "card-items-two">
+            <div class="card-two">
+        <div class="card-content"> 
+        <h2 class="card-title-two">Shirt-x</h2>
+        <p class="card-body-two"> Welcome to Shirt X, the premier destination for all your clothing needs! Our store offers a wide selection of stylish and comfortable apparel for men, women, and children. Whether you’re looking for a casual t-shirt, a dress shirt for a special occasion, or something in between, we have something for everyone. We also carry a variety of accessories, including hats, scarves, and jewelry.</p>
+        </div>
+        </div>
+        </div>
+
+        <div className = "card-items">
+            <div class="card-three">
+        <div class="card-content-three"> 
+        <h2 class="card-title-three">peter</h2>
+        <p class="card-body-three"> Welcome to Shirt X, the premier destination for all your clothing needs! Our store offers a wide selection of stylish and comfortable apparel for men, women, and children. Whether you’re looking for a casual t-shirt, a dress shirt for a special occasion, or something in between, we have something for everyone. We also carry a variety of accessories, including hats, scarves, and jewelry.</p>
+        </div>
+        </div>
+        </div>
+</div>
+
+                    </div>
+            
     )
 }
