8a26dae90b42604bace985c2beea332a2a9a8124
osman <osman.ahmed.2022o@gmail.com>
2023-02-28 01:36:29 +0200

    edit non

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import './mobile.css'
-
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Work from './components/Work';
 import Footer from './components/Footer';
 import Skills from './components/Skills';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { Route, Routes} from 'react-router';
 
+
+Aos.init()
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
-        <Home />
-        <Work />
+
+
+
+
+<Navbar />
+<Routes>
+  
+       <Route path='/' element={<Home />}/>
+      <Route path='/Work' element= {<Work/>}/>
+      
         <Skills />
+    </Routes>
         <Footer />
+       
     </div>
   );
 }
